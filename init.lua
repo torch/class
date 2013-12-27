d436a9a2386c459e803dd59d5ea2a18cde658c5b
Ronan Collobert <ronan@collobert.com>
2013-12-27 14:40:53 +0100

    added support for typename

--- init.lua
+++ init.lua
@@ -84,4 +84,35 @@ class.constructor =
    constructor
 )
 
+class.type =
+   function(obj)
+      local tname = type(obj)
+      if tname == 'table' then
+         return obj.__typename or tname
+      end
+      return tname
+   end
+
+class.istype =
+   function(obj, typename)
+      local tname = type(obj)
+      if tname == 'table' and typename ~= 'table' then
+         if obj.__typename then
+            obj = getmetatable(obj)
+            while type(obj) == 'table' do
+               if obj.__typename == typename then
+                  return true
+               else
+                  obj = getmetatable(obj)
+               end
+            end
+            return false
+         else
+            return tname == typename
+         end
+      else
+         return typename == tname
+      end
+   end
+
 return class
